7b7a5a8085e650d49f1f850b701f899bcea7b6b0
Daniel <info@danielhospitality.nl>
2021-01-21 16:47:18 +0100

    test youtube

--- src/pages/Films.js
+++ src/pages/Films.js
@@ -5,12 +5,19 @@ import Banner from "../components/Banner";
 import Row from "../Row";
 import requests from "../requests";
 
+import YouTube from "react-youtube";
+// import movieTrailer from "movie-trailer";
+
 
 
 function Films() {
   return (
     <div className="app <br/>">
       <Banner />
+      <YouTube />
+
+      
+
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
@@ -18,17 +25,9 @@ function Films() {
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
       <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
       <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
-
-      {/* <h1>Mijn lijst </h1>
-
-      <h1>Top 10 van films in Nederland vandaag </h1>
-
-      <h1>Trending </h1>
-
-      <h1>Thrillers </h1>
-
-      <h1>Netflix Orgiginals </h1> */}
     </div>
+
+    
   );
 }
 
