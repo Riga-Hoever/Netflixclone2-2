d0371470f9009a3816632a6fda5edeef5a9ee2fc
Abraham Engel <engelenburg1980@gmail.com>
2021-01-24 16:23:05 +0100

    Import backend client, fetch data from backend

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,19 +1,24 @@
 import React, { useState, useEffect } from "react";
-import axios from "../axios";
-import requests from "../requests";
+// import axios from "../axios";
+// import requests from "../requests";
+import Backend from "../Backend";
+
 import "./Banner.css";
 
 function Banner() {
   const [movie, setMovie] = useState();
   useEffect(() => {
     async function fetchData() {
-      const request = await axios.get(requests.fetchNetflixOriginals);
-      setMovie(
-        request.data.results[
-          Math.floor(Math.random() * request.data.results.length - 1)
-        ]
-      );
-      return request;
+      // const request = await axios.get(requests.fetchNetflixOriginals);
+      // setMovie(
+      //   request.data.results[
+      //     Math.floor(Math.random() * request.data.results.length - 1)
+      //   ]
+      // );
+      // return request;
+
+      const response = await Backend.fetchMovies(550);
+      setMovie(response.data);
     }
     fetchData();
   }, []);
@@ -22,22 +27,26 @@ function Banner() {
     //... na 200 karakters in description//
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
   }
-  console.table(movie);
   return (
     <header
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdropPath}")`,
         backdropPosition: "center center",
       }}
     >
       {/* Background image */}
       <div className="banner_contents">
         {/* title */}
-        <h1 className="banner_title">
+        {/* <h1 className="banner_title">
           {movie?.title || movie?.name || movie?.original_name}
-        </h1>
+        </h1> */}
+        <img
+          className="logo"
+          src={movie?.hdmovielogo[0].url}
+          alt={movie?.title + "logo"}
+        />
 
         {/* 2 buttons */}
         <div className="banner_buttons">
