a8a2e35123810c75cd4841cb1bb871a4da4f4ea3
Abraham Engel <engelenburg1980@gmail.com>
2021-01-25 11:23:57 +0100

    Add Trailer feature, replace movieTitle with movieLogo

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
-// import axios from "../axios";
-// import requests from "../requests";
 import Backend from "../Backend";
+import YouTube from "react-youtube";
 
 import "./Banner.css";
 
 function Banner() {
   const [movie, setMovie] = useState();
+  const [play, setPlay] = useState();
   useEffect(() => {
     async function fetchData() {
       // const request = await axios.get(requests.fetchNetflixOriginals);
@@ -27,6 +27,17 @@ function Banner() {
     //... na 200 karakters in description//
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
   }
+
+  function playTrailer() {
+    setPlay(true);
+  }
+
+  function onPlayerReady(event) {
+    event.target.playVideo();
+    event.target.h.requestFullscreen();
+    console.log(arguments);
+  }
+  console.log(movie);
   return (
     <header
       className="banner"
@@ -38,10 +49,8 @@ function Banner() {
     >
       {/* Background image */}
       <div className="banner_contents">
-        {/* title */}
-        {/* <h1 className="banner_title">
-          {movie?.title || movie?.name || movie?.original_name}
-        </h1> */}
+
+        {/* movie logo*/}
         <img
           className="banner_movielogo"
           src={movie?.hdmovielogo[0].url}
@@ -50,14 +59,16 @@ function Banner() {
 
         {/* 2 buttons */}
         <div className="banner_buttons">
-          <button className="banner_button">Play</button>
+          <button className="banner_button" onClick={playTrailer}>Play</button>
           <button className="banner_button">Meer informatie </button>
         </div>
 
         {/* description */}
         <h1 className="banner_description">{truncate(movie?.overview, 200)}</h1>
       </div>
-      <div className="banner_fadeBottom" />
+      <div className="banner_fadeBottom"/>
+
+      {play && <YouTube videoId={movie?.youtubeKey} onReady={onPlayerReady}/>}
     </header>
   );
 }
