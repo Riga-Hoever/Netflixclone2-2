fdf455418b20ed2d0a79bf54b60601159f7577d2
Abraham Engel <engelenburg1980@gmail.com>
2021-01-28 20:55:48 +0100

    Add rows

--- src/App.js
+++ src/App.js
@@ -48,19 +48,22 @@ function Home() {
   return (
     <div className="app">
       <Banner />
-      <Row
-        title="NETFLIX ORIGINALS"
-        fetchUrl={requests.fetchNetflixOriginals}
-        
-      />
 
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} isLargeRow />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <div className="row-container">
+          <Row
+            title="NETFLIX ORIGINALS"
+            fetchUrl={requests.fetchNetflixOriginals}
+            />
+
+          <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow />
+          <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+          <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+          <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+          <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} isLargeRow />
+          <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+          <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+        </div>
+
     </div>
   );
 }
