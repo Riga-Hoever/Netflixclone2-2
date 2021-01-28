e740e6e38709f083d80d193eab104090842c77ad
Abraham Engel <engelenburg1980@gmail.com>
2021-01-28 13:39:50 +0100

    Import react player, add custom functions and YouTube player options

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Backend from "../Backend";
 import YouTube from "react-youtube";
+import ReactPlayer from 'react-player';
 
 import "./Banner.css";
 
@@ -32,45 +33,71 @@ function Banner() {
     setPlay(true);
   }
 
+  function toggleColor() {
+    const nav = document.querySelector(".nav_container");
+    nav.style.background = "green";
+  }
+
   function onPlayerReady(event) {
     event.target.playVideo();
     event.target.h.requestFullscreen();
     console.log(arguments);
   }
   console.log(movie);
-  return (
-    <header
-      className="banner"
-      style={{
-        backgroundSize: "cover",
-        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdropPath}")`,
-        backdropPosition: "center center",
-      }}
-    >
-      {/* Background image */}
-      <div className="banner_contents">
 
-        {/* movie logo*/}
+  function playing() {
+    setPlay(true);
+  }
+
+  return (
+    <header className="banner">
+          <div className='player-wrapper'>
+            <ReactPlayer  
+              className='react-player'
+              url={`https://youtu.be/${movie?.youtubeKey}`}
+              light={`https://image.tmdb.org/t/p/original${movie?.backdropPath}`}
+              controls={false}
+              playbackRate = {1}
+              width = "100%"
+              height = "100%"
+              muted={true}
+              loop={true}
+              config={{
+                youtube: {
+                  playerVars: {
+                    disable: 0,
+                    autoplay: 1,
+                    playsinline: 1,
+                    showinfo: 0,
+                    rel: 0,
+                    iv_load_policy: 3,
+                    fs: 0,
+                  }
+                } 
+              }}
+              onPlay={toggleColor}
+ 
+
+/>
+          </div>
+      
+      <div className="banner_contents">
         <img
           className="banner_movielogo"
           src={movie?.logoUrl}
           alt={movie?.title + "logo"}
         />
-
-        {/* 2 buttons */}
         <div className="banner_buttons">
           <button className="banner_button" onClick={playTrailer}>Play</button>
           <button className="banner_button">Meer informatie</button>
         </div>
-
-        {/* description */}
         <h1 className="banner_description">{truncate(movie?.overview, 200)}</h1>
       </div>
+
       <div className="banner_fadeBottom"/>
 
-      {play && <YouTube videoId={movie?.youtubeKey} onReady={onPlayerReady}/>}
     </header>
+    
   );
 }
-
 export default Banner;
